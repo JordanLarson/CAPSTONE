34e8d37ed6da257af94fbeaf87af2ebc2b918f63
JordanLarson <jdlarson1234@gmail.com>
2020-08-16 19:13:24 -0700

    tweaking wave data

--- surf-app/src/components/Waves/Waves.jsx
+++ surf-app/src/components/Waves/Waves.jsx
@@ -4,7 +4,8 @@ import cors from "cors";
 import { Link } from "react-router-dom";
 
 const Waves = (props) => {
-  const [waves, setWaves] = useState("");
+  const [maxWaveHeight, setMaxWaveHeight] = useState("");
+  const [minWaveHeight, setMinWaveHeight] = useState("");
   const [favorite, setFavorite] = useState(false);
   const [spotId, setSpotId] = useState();
 
@@ -17,15 +18,17 @@ const Waves = (props) => {
       const urlData = `http://magicseaweed.com/api/66c79af0fe4e3fb73b3915ea2ef63999/forecast/?spot_id=291`;
       const res = await fetch(urlData);
       const data = await res.json();
-      console.log(data[1].swell.absMaxBreakingHeight);
-      setWaves(data[1].swell.absMaxBreakingHeight);
+      console.log(data[1].swell);
+      setMaxWaveHeight(data[1].swell.absMaxBreakingHeight);
+      setMinWaveHeight(data[1].swell.absMinBreakingHeight);
     };
     makeApiCall();
   }, []);
   return (
     <div>
       <h3>Waves Nearby</h3>
-      <p>wave height: {waves}</p>
+      <p>Maximum wave height: {maxWaveHeight} feet</p>
+      <p>Minimum Wave Height: {minWaveHeight} feet</p>
       <button onClick={handleSubmit}>
         <span>Add To Your Waves</span>
         {/* <p>data[1].swell.absMaxBreakingHeight</p> */}
