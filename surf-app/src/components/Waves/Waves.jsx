b611b5b3f76e41cf7ad2b6dc1eb094174bdd2491
JordanLarson <jdlarson1234@gmail.com>
2020-08-16 19:20:29 -0700

    more data rendering wahoo

--- surf-app/src/components/Waves/Waves.jsx
+++ surf-app/src/components/Waves/Waves.jsx
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 const Waves = (props) => {
   const [maxWaveHeight, setMaxWaveHeight] = useState("");
   const [minWaveHeight, setMinWaveHeight] = useState("");
+  const [waterTemp, setWaterTemp] = useState("");
+  const [solidRating, setSolidRating] = useState("");
   const [favorite, setFavorite] = useState(false);
   const [spotId, setSpotId] = useState();
 
@@ -18,9 +20,11 @@ const Waves = (props) => {
       const urlData = `http://magicseaweed.com/api/66c79af0fe4e3fb73b3915ea2ef63999/forecast/?spot_id=291`;
       const res = await fetch(urlData);
       const data = await res.json();
-      console.log(data[1].swell);
-      setMaxWaveHeight(data[1].swell.absMaxBreakingHeight);
-      setMinWaveHeight(data[1].swell.absMinBreakingHeight);
+      console.log(data[0].solidRating);
+      setMaxWaveHeight(data[0].swell.absMaxBreakingHeight);
+      setMinWaveHeight(data[0].swell.absMinBreakingHeight);
+      setWaterTemp(data[0].condition.temperature);
+      setSolidRating(data[0].solidRating);
     };
     makeApiCall();
   }, []);
@@ -29,6 +33,8 @@ const Waves = (props) => {
       <h3>Waves Nearby</h3>
       <p>Maximum wave height: {maxWaveHeight} feet</p>
       <p>Minimum Wave Height: {minWaveHeight} feet</p>
+      <p>Current Water Temp: {waterTemp} degrees</p>
+      <p>Solid Rating:{solidRating}/5</p>
       <button onClick={handleSubmit}>
         <span>Add To Your Waves</span>
         {/* <p>data[1].swell.absMaxBreakingHeight</p> */}
