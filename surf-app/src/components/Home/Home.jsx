a4d2a454f1f40a5619484fe6f549281a45e8fe7b
JordanLarson <jdlarson1234@gmail.com>
2020-08-18 16:58:30 -0700

    logo added and styling cont'd

--- surf-app/src/components/Home/Home.jsx
+++ surf-app/src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <div>
       <h1 className="home-header">
+        <img src="https://iili.io/dO8Dhu.png" alt="dO8Dhu.png" border="0" />
         FIND THE BEST <br></br>SURF IN<br></br>YOUR REGION
       </h1>
       <Login />
