8346639e407b09b5201a191b7f7ec5715e10ca5f
JordanLarson <jdlarson1234@gmail.com>
2020-08-18 21:24:34 -0700

    progress

--- surf-app/src/components/Favorites/Favorites.jsx
+++ surf-app/src/components/Favorites/Favorites.jsx
@@ -4,6 +4,21 @@ import Axios from "axios";
 import apiUrl from "../apiConfig";
 
 const Favorites = (props) => {
+  const [favoriteSpot, setFavoriteSpot] = useState({});
+  useEffect(() => {
+    let urlData = `${apiUrl}/users/${id}`;
+  });
+  //   const renderSpots = () => {
+  //     if (spots == null || spots[0] == null) {
+  //       return;
+  //     }
+
+  //     return spots.map((spot) => (
+  //       <div key={spot.id}>
+  //         <Link to={`/messages/${spot.id}`}>{spot.name}</Link>
+  //       </div>
+  //     ));
+  //   };
   return (
     <div>
       <h2 className="favorites-header">Favorites</h2>
